00dd385ef1e1a16a83beabe7adb47d0c9fd58cbb
swaglord2021 <nuoya1996@gmail.com>
2021-06-14 22:10:58 -0400

    remove awake.js

--- awake.js
+++
@@ -1,9 +0,0 @@
-const express = require('express')
-
-const app = express()
-
-const http = require('http').Server(app)
-
-setInterval(() => {
-    http.get("https://songs-player.herokuapp.com/")
-}, 1500000)
