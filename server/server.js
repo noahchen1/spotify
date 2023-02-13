a67e5bac69839f46c719a7efe57a636f4d7aeaad
swaglord2021 <nuoya1996@gmail.com>
2023-02-13 16:43:43 -0500

    updated cors.

--- server/server.js
+++ server/server.js
@@ -9,7 +9,7 @@ const path = require('path')
 const app = express()
 const port = process.env.PORT || 3001
 app.use(express.json());
-app.use(cors(allowedOrigins));
+app.use(cors());
 
 app.post("/refresh", (req, res) => {
     const refreshToken = req.body.refreshToken
